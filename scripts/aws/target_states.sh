5e8dcdb5b81f8ad1bc93b18c0eb99fcd852b8d8e
aidan.fitzpatrick <aidan.fitzpatrick@youlend.com>
2021-06-08 23:30:09 +0100

    fix: master: neating up target_states

--- scripts/aws/target_states.sh
+++ scripts/aws/target_states.sh
@@ -3,9 +3,12 @@
 HOST_HEALTH=$1
 
 for i in $(aws elbv2 describe-target-groups --query TargetGroups[*].TargetGroupArn | cut -d '"' -f2 | grep -v "\[\|\]"); do
-  INSTANCES=$(aws elbv2 describe-target-health --target-group-arn $i --query TargetHealthDescriptions[*].[Target.Id,TargetHealth.State] --output text | grep "$HOST_HEALTH" | cut -f1 | grep -v "\[\|\]")
-  #if [ $(echo $INSTANCES | tr -dc '[:print:]') -eq 00000000]
+  case $HOST_HEALTH in
+    healthy) INSTANCES=$(aws elbv2 describe-target-health --target-group-arn $i --query TargetHealthDescriptions[*].[Target.Id,TargetHealth.State] --output text | grep -v unhealthy | cut -f1 | grep -v "\[\|\]") ;;
+    unhealthy) INSTANCES=$(aws elbv2 describe-target-health --target-group-arn $i --query TargetHealthDescriptions[*].[Target.Id,TargetHealth.State] --output text | grep unhealthy | cut -f1 | grep -v "\[\|\]") ;;
+    *) INSTANCES=$(aws elbv2 describe-target-health --target-group-arn $i --query TargetHealthDescriptions[*].[Target.Id,TargetHealth.State] --output text | cut -f1 | grep -v "\[\|\]") ;;
+  esac
   if [[ ! -z $INSTANCES ]]
-    then echo $i $INSTANCES # | tr -dc '[:print:]' #| od -c
+    then echo $i $INSTANCES
   fi
 done
